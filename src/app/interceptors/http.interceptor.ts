1130e27bc6133cbebc301d6d00b3d7da3104605c
GersonBenito <gersonbenito4@gmail.com>
2025-04-13 01:13:44 -0600

    feat: interceptor view log in requet added

--- src/app/interceptors/http.interceptor.ts
+++ src/app/interceptors/http.interceptor.ts
@@ -2,5 +2,23 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
   console.log(`[${req.method}][${req.url}]`);
+  /*
+    Todas las peticiones pasan por el interceptor 
+    por lo que es necesario omitir el token en caso de que la url sea publico
+    para el consumo de los assets
+  */
+  if(!req.url.includes('assets')){
+    const token = 'YOUR_TOKEN';
+  
+    const reqClone = req.clone({
+      setHeaders: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    
+    return next(reqClone);
+  }
+
+
   return next(req);
 };
